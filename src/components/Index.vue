e468f2050fa3e2e7d65d8a87d85e639bf367612b
Wjinlei <1976883731@qq.com>
2020-08-06 22:43:44 +0800

    技术支持

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -56,34 +56,34 @@
             <a-icon class="ma5 color-primary" type="tag" />数据概览
           </span>
           <a-col :xs="24" :sm="24" :md="12" :lg="6" :xl="4">
-            <a-card class="bgf8 text-center" :bordered="false">
-              <p>
+            <a href="#">
+              <a-card class="bgf8 text-center hover" :bordered="false">
+                <p>
                   <a-icon class="ma5" type="global" />网站
-              </p>
-              <p class="font22px color-primary">
-                <a href="#">0</a>
-              </p>
-            </a-card>
+                </p>
+                <p class="font22px color-primary">0</p>
+              </a-card>
+            </a>
           </a-col>
           <a-col :xs="24" :sm="24" :md="12" :lg="6" :xl="4">
-            <a-card class="bgf8 text-center" :bordered="false">
-              <p>
+            <a href="#">
+              <a-card class="bgf8 text-center hover" :bordered="false">
+                <p>
                   <a-icon class="ma5" type="database" />数据库
-              </p>
-              <p class="font22px color-primary">
-                <a href="#">0</a>
-              </p>
-            </a-card>
+                </p>
+                <p class="font22px color-primary">0</p>
+              </a-card>
+            </a>
           </a-col>
           <a-col :xs="24" :sm="24" :md="12" :lg="6" :xl="4">
-            <a-card class="bgf8 text-center" :bordered="false">
-              <p>
+            <a href="#">
+              <a-card class="bgf8 text-center hover" :bordered="false">
+                <p>
                   <a-icon class="ma5" type="file-protect" />SSL
-              </p>
-              <p class="font22px color-primary">
-                <a href="#">0</a>
-              </p>
-            </a-card>
+                </p>
+                <p class="font22px color-primary">0</p>
+              </a-card>
+            </a>
           </a-col>
         </a-card>
       </a-col>
@@ -116,9 +116,54 @@
           <span slot="title">
             <a-icon class="ma5 color-primary" type="customer-service" />技术支持
           </span>
-          <p>card content</p>
-          <p>card content</p>
-          <p>card content</p>
+          <a-col :xs="24" :sm="24" :md="12" :lg="6" :xl="4">
+            <a href="http://t.hws.com" target="_blank">
+              <a-card class="bgf8 text-left hover" :bordered="false">
+                <p>
+                  <a-icon type="message" />论坛
+                </p>
+                <p class="color-primary font12px">有任何问题，请上论坛发帖求助。</p>
+              </a-card>
+            </a>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="12" :lg="6" :xl="4">
+            <a href="https://www.hws.com" target="_blank">
+              <a-card class="bgf8 text-left hover" :bordered="false">
+                <p>
+                  <a-icon type="star" />官网
+                </p>
+                <p class="color-error font12px">服务器安全，认准护卫神。</p>
+              </a-card>
+            </a>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="12" :lg="6" :xl="4">
+            <a-card class="bgf8 text-left hover" :bordered="false">
+              <p>
+                <a-icon type="qq" />QQ：
+                <a href="#" target="_blank">800181978</a>
+              </p>
+              <p class="color-success font12px">客服QQ，24小时在线为您服务。</p>
+            </a-card>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="12" :lg="6" :xl="4">
+            <a-card class="bgf8 text-left hover" :bordered="false">
+              <p>
+                <a-icon type="phone" />电话：
+                <a href="#" target="_blank">028-89549999</a>
+              </p>
+              <p class="color-warning font12px">电话沟通，更方便，更快捷。</p>
+            </a-card>
+          </a-col>
+          <a-col :xs="24" :sm="24" :md="12" :lg="6" :xl="4">
+            <a href="https://www.hws.com/help/linuxmaster/" target="_blank">
+              <a-card class="bgf8 text-left hover" :bordered="false">
+                <p>
+                  <a-icon type="solution" />帮助
+                </p>
+                <p class="color-primary font12px">帮助文档、教程、知识分享。</p>
+              </a-card>
+            </a>
+          </a-col>
         </a-card>
       </a-col>
     </a-row>
@@ -152,3 +197,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.hover:hover {
+  background-color: #ededed;
+}
+</style>
